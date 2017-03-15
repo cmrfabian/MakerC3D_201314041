06d8ee2d64acf256cdfb4b953c96060122bf24d3
Christian <chrisfabian594@gmail.com>
2017-03-15 16:00:08 -0600

    HOJA DE TRABAJO 2 ARREGLADA

--- src/org/compi2/codigo3d/parser/NodoC3D.java
+++ src/org/compi2/codigo3d/parser/NodoC3D.java
@@ -8,13 +8,32 @@ package org.compi2.codigo3d.parser;
  */
 public class NodoC3D {
     private String cad;
+    private String LV;
+    private String LF;
 
     public NodoC3D(String cad) {
         this.cad = cad;
     }
     
+    public NodoC3D(String lv, String lf){
+        this.LV = lv;
+        this.LF = lf;
+    }
+    
+    
+    
     public String getCad(){
         return cad;
     }
+    
+    public String getLV(){
+        return LV;
+    }
+        
+    public String getLF(){
+        return LF;
+    }
+    
+    
         
 }
